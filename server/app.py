98466a41412f0c6be7b92974534d7486260b0342
GlenWamaru <garpirungu@gmail.com>
2024-04-13 15:34:06 +0300

    Initial commit

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, make_response, jsonify, request, session
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource 
 
 from models import db, Article, User
 
@@ -48,9 +48,34 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        user = User.query.filter(User.username == data['username']).first()
+
+        if user and user.password == data['password']:
+            session['user_id'] = user.id
+            return jsonify(user.to_dict()), 200   
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return '', 204
+
+class CheckSession(Resource):
+    def get(self):
+        if 'user_id' in session:
+            user = User.query.get(session['user_id'])
+            return jsonify(user.to_dict()), 200
+
+        return '', 401
+
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 
 if __name__ == '__main__':
